e3d0def32667fb29dbbde7a0b1509d2e0b5ee86a
Jake Wymer <jakewymer@att.net>
2018-06-26 14:15:18 -0500

    completed project

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post';
 
 class App extends Component {
   constructor() {
@@ -18,24 +20,37 @@ class App extends Component {
     this.createPost = this.createPost.bind( this );
   }
   
-  componentDidMount() {
-
+  async componentDidMount() {
+    let result = await axios.get('https://practiceapi.devmountain.com/api/posts');
+    this.setState({posts: result.data});
   }
 
-  updatePost() {
-  
+  async updatePost(id, text) {
+    let result = await axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text});
+    this.setState({posts: result.data});
   }
 
-  deletePost() {
-
+  async deletePost(id) {
+    let result = await axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`);
+    this.setState({posts: result.data});
   }
 
-  createPost() {
-
+  async createPost(text) {
+    let result = await axios.post('https://practiceapi.devmountain.com/api/posts', {text});
+    this.setState({posts: result.data});
   }
 
   render() {
     const { posts } = this.state;
+    let list = posts.map((e, i) => {
+      return <Post 
+                key={i} 
+                text={e.text}
+                date={e.date}
+                updatePostFn={this.updatePost}
+                deletePostFn={this.deletePost}
+                id={e.id}/>
+    });
 
     return (
       <div className="App__parent">
@@ -43,8 +58,9 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose 
+            createPostFn={this.createPost}/>
+          {list}
         </section>
       </div>
     );
